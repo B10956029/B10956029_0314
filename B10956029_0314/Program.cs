c35d2686e49267c10c3bf758eff902c68c654887
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:46:39 +0800

    000

--- B10956029_0314/Program.cs
+++ B10956029_0314/Program.cs
@@ -10,6 +10,99 @@ namespace B10956029_0314
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("請輸入數字");
+            //String input = Console.ReadLine();
+            //int n = int.Parse(input);
+            //for( var A=0; A < n; A++)
+            //{
+            //    for (var A1 = 0;  A1 < A; A1++)
+            //    {
+            //        Console.WriteLine("*");
+            //    }
+            //   Console.WriteLine();
+            //}
+
+            //Console.WriteLine("請輸入數字");
+            //String N1 = Console.ReadLine();
+            //int N = int.Parse(N1);
+            //Console.WriteLine(N+"*1="+(N * 1));
+            //Console.WriteLine(N+"*1="+(N * 2));
+            //Console.WriteLine(N+"*1="+(N * 3));
+
+            //Console.WriteLine("九九乘法表");
+            //for (int i=1;i<10;i++)
+            //{
+            //    for (int j = 1; j < 10; j++)
+            //    {
+            //        Console.WriteLine(i+"*"+j+"="+(i*j)); 
+            //    }
+            //}
+
+
+            //int a = 5;
+
+            //if (a%2==1)
+            //{
+            //    Console.WriteLine("Weird");
+            //}
+            //else if(a<2 && a>5)
+            //{
+            //    Console.WriteLine("Not Weird");
+            //}
+            //else if(a < 6 && a > 20)
+            //{
+            //    Console.WriteLine("Weird");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Weird");
+            //}
+
+            //switch (a) 
+            //{
+            //    case 5:
+            //        Console.WriteLine("Weird");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Weird");
+            //        break;
+            //}
+
+
+            //for (int b=1;b<=10;b++)
+            //{
+            //    Console.WriteLine("Weird");
+            //}
+
+            //int i= 0;
+            //while(i<=10)
+            //{
+            //    Console.WriteLine("Weird");
+            //    i++;
+            //}
+
+
+            //try
+            //{
+            //    //......
+            //    Console.WriteLine("Weird");
+            //}
+            //catch(Exception e)
+            //{
+            //    //記錄錯誤
+            //}
+
+            //int[] arr = { 1, 3, 5, 7 };
+            //Console.WriteLine(arr.Length);
+
+            Console.WriteLine("請輸入數字");
+            String input = Console.ReadLine();
+
+           
+
+            Console.ReadLine();
+
         }
     }
 }
