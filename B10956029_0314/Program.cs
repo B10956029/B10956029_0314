b502b091bb0255cc2ba52d485a3ddd89e18604f9
user <user@DESKTOP-6JBEF5K>
2022-03-14 12:05:29 +0800

    888

--- B10956029_0314/Program.cs
+++ B10956029_0314/Program.cs
@@ -10,6 +10,36 @@ namespace B10956029_0314
     {
         static void Main(string[] args)
         {
+
+            int[] studentNo = {1,2,3,4,5};
+            string[] studentName = {"Jack","Mary","Tom","Grace","Alice"};
+            int[] computerScore = {80,65,100,98,83};
+            int[] progarmScore = {75,67,93,25,82};
+            int[] animationScore = {60,62,91,50,87};
+
+            while (true)
+            {
+                Console.WriteLine("請輸入座號");
+                String input = Console.ReadLine();
+                int inputstudentNo = int.Parse(input);
+                int arrayIndex = Array.IndexOf(studentNo, inputstudentNo);
+
+                if (arrayIndex>-1)
+                {
+                    Console.WriteLine("姓名:"+studentName[arrayIndex]);
+                    Console.WriteLine("電腦概論:"+ computerScore[arrayIndex]);
+                    Console.WriteLine("程式設計:"+ progarmScore[arrayIndex]);
+                    Console.WriteLine("動畫設計:"+ animationScore[arrayIndex]);
+                }
+                else
+                {
+                    Console.WriteLine("沒有座號");
+                }
+            }
+            
+
+            Console.ReadLine();
+
             //Console.WriteLine("請輸入數字");
             //String input = Console.ReadLine();
             //int n = int.Parse(input);
@@ -96,12 +126,7 @@ namespace B10956029_0314
             //int[] arr = { 1, 3, 5, 7 };
             //Console.WriteLine(arr.Length);
 
-            Console.WriteLine("請輸入數字");
-            String input = Console.ReadLine();
-
-           
-
-            Console.ReadLine();
+            
 
         }
     }
