f4e58baa81a87a574b6b85cf47cc1373a4ffd7f0
user <user@DESKTOP-6JBEF5K>
2022-03-14 09:27:45 +0800

    難

---
+++ B10956029_0314/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B10956029_0314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
